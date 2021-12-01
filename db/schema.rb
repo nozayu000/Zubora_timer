e008ca16e912174587bb8ef3cf27d343e3e91c36
nozayu000 <n.yuntamu@gmail.com>
2021-12-01 14:07:08 +0000

    devise users　消す

--- db/schema.rb
+++ db/schema.rb
@@ -70,23 +70,4 @@ ActiveRecord::Schema.define(version: 2021_11_28_033236) do
     t.integer "rest_seconds"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "nickname"
-    t.integer "age"
-    # t.string "last_name"
-    # t.string "first_name"
-    t.boolean "is_deleted"
-    t.boolean "public"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
 end
